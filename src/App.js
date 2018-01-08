17d2b7d64cd3f88d15d08f6adb4eb6485aac50b3
QiangZeng <qzeng2017@163.com>
2018-01-08 17:11:42 +0800

    实现GET API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Employee extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.employee.firstName}</td>
+				<td>{this.props.employee.lastName}</td>
+				<td>{this.props.employee.description}</td>
+			</tr>
+		)
+	}
+}
+
+class EmployeeList extends React.Component{
+	render() {
+		var employees = this.props.employees.map(employee =>
+			<Employee key={employee._links.self.href} employee={employee}/>
+		);
+		return (
+      <div className="container">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>{employees}</tbody>
+        </table>
+      </div>
+			
+		)
+	}
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+  constructor(props) {
+		super(props);
+		this.state = {employees: []};
+	}
+
+	componentDidMount() {
+		
+    var $that = this;
+    fetch('http://localhost:8081/api/employees').then(function(response) {
+      // $that.setState({employees: response.entity._embedded.employees});
+      return response.json();
+    }).then(function(res){
+      $that.setState({employees: res._embedded.employees});
+      // console.log(res._embedded.employees);
+    })
+	}
+
+	render() {
+		return (
+			<EmployeeList employees={this.state.employees}/>
+		)
+	}
 }
 
 export default App;
